126edf31a926a2aa8e409f4c3be9d27f38c86d15
Artem Sharov <ash.03@yandex.ru>
2021-12-05 03:03:05 -0800

    vivat

---
+++ include/student.hpp
@@ -0,0 +1,29 @@
+// Copyright 2021 Your Name <your_email>
+
+#ifndef INCLUDE_STUDENT_HPP_
+#define INCLUDE_STUDENT_HPP_
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <any>
+#include <vector>
+#include <stdexcept>
+#include <nlohmann/json.hpp>
+#include <iomanip>
+using nlohmann::json;
+
+struct Student
+{
+  std::string name;
+  std::any group;
+  std::any avg;
+  std:: any debt;
+};
+json collect_inform(const std::string &path);
+void make_stud(const json &data, Student &st);
+std::vector<Student> parse_file(json &data);
+void print();
+void print(const Student &student, std::ostream &os);
+void print(std::vector<Student> &students, std::ostream &os);
+#endif // INCLUDE_STUDENT_HPP_
---
+++ tests/test.cpp
@@ -0,0 +1,87 @@
+// Copyright 2021 Your Name <your_email>
+
+#include <gtest/gtest.h>
+#include <student.hpp>
+
+TEST(Test1, parse_file) {
+  Student one = {"Ivanov Petr", (std::string)"1", (std::string) "4.25", nullptr};
+  Student two = {"Sidorov Ivan", (size_t)31, (size_t)4, (std::string)"C++" };
+  std::vector<std::string> debts = {"C++", "Linux", "Network"};
+  Student three = {"Petrov Nikita", (std::string)"IU8-31", (double)3.33,
+                   (std::vector<std::string>)debts};
+  std::vector<Student> students_1 = {one, two, three};
+  std::stringstream ss1;
+  print(students_1, ss1);
+
+  auto text = R"(
+{
+  "items": [
+    {
+      "name": "Ivanov Petr",
+      "group": "1",
+      "avg": "4.25",
+      "debt": null
+    },
+    {
+      "name": "Sidorov Ivan",
+      "group": 31,
+      "avg": 4,
+      "debt": "C++"
+    },
+    {
+      "name": "Petrov Nikita",
+      "group": "IU8-31",
+      "avg": 3.33,
+      "debt": [
+        "C++",
+        "Linux",
+        "Network"
+      ]
+    }
+  ],
+  "_meta": {
+    "count": 3
+  }
+})";
+  std::stringstream ss;
+  ss << text;
+  nlohmann::json data = json::parse(ss);
+  std::vector<Student> students_2 = parse_file(data);
+  std::stringstream ss2;
+  print(students_2, ss2);
+
+  ASSERT_EQ(ss1.str(), ss2.str());
+
+}
+TEST(Test2, correct_types_1){
+  const std::string data_str ="{\n"
+      "      \"name\": \"Ivanov Petr\",\n"
+      "      \"group\": 1,\n"
+      "      \"avg\": 4.25,\n"
+      "      \"debt\": [\n"
+      "        \"C++\",\n"
+      "        \"Linux\",\n"
+      "        \"Network\"\n"
+      "      ]\n"
+      "    }";
+  nlohmann::json data = json::parse(data_str);
+  Student st;
+  make_stud(data, st);
+  EXPECT_TRUE(st.group.type()==typeid(std::size_t));
+  EXPECT_TRUE(st.avg.type()==typeid(double));
+  EXPECT_TRUE(st.debt.type()== typeid(std::vector<std::string>));
+}
+TEST(Test3, correct_types_2){
+  const std::string data_str ="{\n"
+      "      \"name\": \"Ivanov Petr\",\n"
+      "      \"group\": \"IU8-31\",\n"
+      "      \"avg\": 4,\n"
+      "      \"debt\": null\n"
+      "    }";
+  nlohmann::json data = json::parse(data_str);
+  Student st;
+  make_stud(data, st);
+  EXPECT_TRUE(st.group.type()==typeid(std::string));
+  EXPECT_TRUE(st.avg.type()==typeid(std::size_t));
+  EXPECT_TRUE(st.debt.type()==typeid(nullptr));
+}
